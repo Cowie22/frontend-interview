96f14687442284c7ae095cb5e8b0a0383efd372b
Emerson Hum <emerson.hum@stridehealth.com>
2019-04-18 14:09:23 -0700

    Update readme, remove starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import _ from 'lodash';
+import axios from 'axios';
 import './App.css';
+
+const API_URL = 'https://data.townofcary.org/api/v2/catalog/datasets/parks-and-recreation-feature-map/exports/json';
+const DISPLAY_FIELDS = [
+  'name',
+  'baseball',
+  'basketball',
+  'battingcages',
+  'climbingropes',
+  'dogpark',
+  'fitnesstrail',
+  'openspace',
+  'picnic',
+  'skatepark',
+  'soccer',
+  'tenniscourt',
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Hello, world
       </div>
     );
   }
