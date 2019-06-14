d06ec9b8fd13d6355eea6f4330f1b1622900e2cd
Andrew Van Wormer <a.vwormer@gmail.com>
2019-06-14 13:30:45 -0700

    Complete Stride Health coding challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import _ from 'lodash';
-import axios from 'axios';
+// import _ from 'lodash';
+// import axios from 'axios';
+import Park from './park'
 import './App.css';
 
 const API_URL = 'https://data.townofcary.org/api/v2/catalog/datasets/parks-and-recreation-feature-map/exports/json';
@@ -20,13 +21,72 @@ const DISPLAY_FIELDS = [
 ];
 
 class App extends Component {
+  //may want to set up a state
+  constructor(){
+    super();
+    this.state = {
+      parkData: [],
+      filteredParks: [],
+      filterBoolean: false,
+    }
+    this.filter = this.filter.bind(this)
+  }
+
+  componentDidMount() {
+    //pull data from API
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(res => {
+      //console.log(res)
+      this.setState({
+        parkData: res
+      })
+    })
+    .then(() => console.log(this.state.parkData))
+  }
+
+  filter(event) {
+
+    //create condition to remove filter
+    
+    let name = event.target.innerHTML
+    let filtered = this.state.parkData.filter(ele => {
+      if (ele[name] === 'Yes') {
+        return ele
+      }
+    })
+
+    if(this.state.filterBoolean) {
+      this.setState({
+        filteredParks: []
+      })
+    }
+    
+    this.setState({
+      filterBoolean: true, 
+      filteredParks: filtered
+    })
+  }
+
   render() {
+    let headers = DISPLAY_FIELDS.map(ele => <td onClick={this.filter}>{ele}</td>)
     return (
       <div className="App">
-        Hello, world
+        <table>
+          <thead>
+            <tr>
+              {headers}
+            </tr>
+          </thead>
+          <tbody>
+            <Park state={this.state} />
+          </tbody>
+        </table>
       </div>
     );
   }
 }
+
+
 
 export default App;
---
+++ src/park.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+
+const DISPLAY_FIELDS = [
+    'name',
+    'baseball',
+    'basketball',
+    'battingcages',
+    'climbingropes',
+    'dogpark',
+    'fitnesstrail',
+    'openspace',
+    'picnic',
+    'skatepark',
+    'soccer',
+    'tenniscourt',
+  ];
+
+let Park = (props) => {
+
+    let data = props.state.parkData
+    if (props.state.filterBoolean) {
+        data = props.state.filteredParks
+    }
+
+    let headersWithData = DISPLAY_FIELDS.map(header => {
+        return 
+    })
+
+    let dataWithHeaders = data.map(obj => {
+        return DISPLAY_FIELDS.map(header => {
+            return <td>{obj}[{header}]</td>
+        })
+    })
+
+
+
+    return (
+        //dataWithHeaders
+        data.map(ele => {
+            return (
+            <tr>
+                <td>{ele.name}</td>
+                <td>{ele.baseball}</td>
+                <td>{ele.basketball}</td>
+                <td>{ele.battingcages}</td>
+                <td>{ele.climbingropes}</td>
+                <td>{ele.dogpark}</td>
+                <td>{ele.fitnesstrail}</td>
+                <td>{ele.openspace}</td>
+                <td>{ele.picnic}</td>
+                <td>{ele.skatepark}</td>
+                <td>{ele.soccer}</td>
+                <td>{ele.tenniscourt}</td>
+            </tr>
+            )  
+        })
+    )
+}
+
+export default Park;
